638b616cb1bfa1f93b2e4f524611be39c950e2b3
jim60105 <jim60105@gmail.com>
2019-03-23 17:14:07 +0800

    v19.03.23.1 New: 1.Get UI List from game data

--- KK_StudioCoordinateLoadOption/CostumeInfo_Init_Patches.cs
+++ KK_StudioCoordinateLoadOption/CostumeInfo_Init_Patches.cs
@@ -7,6 +7,7 @@ using ToolBox;
 using UILib;
 using UnityEngine;
 using UnityEngine.UI;
+using Illusion.Game;
 
 namespace KK_StudioCoordinateLoadOption
 {
@@ -22,19 +23,34 @@ namespace KK_StudioCoordinateLoadOption
             Console.WriteLine("KK_SCLO:Patcher Start");
 			CharaFileSort charaFileSort = (CharaFileSort)__instance.GetPrivate("fileSort");
 			Transform parent = charaFileSort.root.parent;
-            //(parent as RectTransform).offsetMin += new Vector2(0f, 18f);
-            int amount = 7;
-            Image panel = UILib.UIUtility.CreatePanel("TooglePanel", parent.parent.parent);
-			panel.transform.SetRect(Vector2.zero, new Vector2(1f, 0f), new Vector2(407f, 330f-amount*20), new Vector2(100f, 340f));
-            panel.GetComponent<Image>().color = new Color32(80,80,80,220);
 
-            for (int i = 0; i < amount; i++) { 
-                Toggle toggleTmp = UILib.UIUtility.CreateToggle("Toggle"+i, panel.transform, "toggle第"+i);
+            Type ClothesKind = typeof(ChaFileDefine.ClothesKind);
+            Array ClothesKindArray = Enum.GetValues(ClothesKind);
+
+            Image panel = UILib.UIUtility.CreatePanel("TooglePanel", parent.parent.parent);
+            Toggle toggleAll = UILib.UIUtility.CreateToggle("ToggleAll", panel.transform, "all");
+            toggleAll.GetComponentInChildren<Text>(true).alignment = TextAnchor.UpperLeft;
+            toggleAll.GetComponentInChildren<Text>(true).color = Color.white;
+            toggleAll.transform.SetRect(Vector2.zero, new Vector2(1f, 0f), new Vector2(5f, 20f+20f*ClothesKindArray.Length), new Vector2(0f, 45f+20f*(ClothesKindArray.Length)));
+            toggleAll.GetComponentInChildren<Text>(true).transform.SetRect(Vector2.zero, new Vector2(1f, 1f), new Vector2(20f, 1f), new Vector2(-5f, -2f));
+
+            for (int i = 0; i < ClothesKindArray.Length; i++) { 
+                Toggle toggleTmp = UILib.UIUtility.CreateToggle("Toggle"+i, panel.transform, ClothesKindArray.GetValue(i).ToString());
                 toggleTmp.GetComponentInChildren<Text>(true).alignment = TextAnchor.UpperLeft;
                 toggleTmp.GetComponentInChildren<Text>(true).color = Color.white;
-                toggleTmp.transform.SetRect(Vector2.zero, new Vector2(1f, 0f), new Vector2(5f, -20f+20f*(amount-i)), new Vector2(0f, 5f+20f*(amount-i)));
+                toggleTmp.transform.SetRect(Vector2.zero, new Vector2(1f, 0f), new Vector2(5f, 20f*(ClothesKindArray.Length-i)), new Vector2(0f, 25f+20f*(ClothesKindArray.Length-i)));
                 toggleTmp.GetComponentInChildren<Text>(true).transform.SetRect(Vector2.zero, new Vector2(1f, 1f), new Vector2(20f, 1f), new Vector2(-5f, -2f));
             }
+
+            Toggle toggleAcc = UILib.UIUtility.CreateToggle("ToggleAcc", panel.transform, "accessories");
+            toggleAcc.GetComponentInChildren<Text>(true).alignment = TextAnchor.UpperLeft;
+            toggleAcc.GetComponentInChildren<Text>(true).color = Color.white;
+            toggleAcc.transform.SetRect(Vector2.zero, new Vector2(1f, 0f), new Vector2(5f, 0f), new Vector2(0f, 25f));
+            toggleAcc.GetComponentInChildren<Text>(true).transform.SetRect(Vector2.zero, new Vector2(1f, 1f), new Vector2(20f, 1f), new Vector2(-5f, -2f));
+
+			panel.transform.SetRect(Vector2.zero, new Vector2(1f, 0f), new Vector2(407f, 290f-ClothesKindArray.Length*20), new Vector2(150f, 340f));
+            panel.GetComponent<Image>().color = new Color32(80,80,80,220);
+
 		}
 
 		private static void SearchUpdated(string text, List<CharaFileInfo> items)
