0a7565f7daff2e515249117459979091f090e03c
陳鈞 <jim60105@gmail.com>
2019-05-16 02:39:30 +0800

    v19.05.16.0 新增KK_SRFF
    目標
    1. IK盤裡的"複製到FK"去掉RightHand, LeftHand, Neck
    2. 在首操作加一個"複製到FK(首)"的功能，等同於上面拔掉的Neck
    3. 在手加一個"複製到FK(手)"的功能，等同於上面拔掉的RightHand, LeftHand
    
    已完成第一項

---
+++ KK_StudioReflectFKFix/KK_StudioReflectFKFix.cs
@@ -0,0 +1,75 @@
+﻿/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMM               MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM    M7    MZ    MMO    MMMMM
+MMM               MMMMMMMMMMMMM   MMM     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMM               MMMMMMMMMM       ?M     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMM8     MMMMMMMM       ~MMM.    MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMM        MMM                 M    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM.         ZMMMMMM     MMMM     MMMMMMMMMMMMZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM      .   ZMMMMMM     MMMM     MMMMMMMMMMMM?    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    $MMMMMM     MMMM     MMMMMMMMMMMM?    MM8    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    7MMMMMM     MMMM     MMMMMMMMMMMMI    MM8    MMMMM
+MMM               MMMMMMMM    7MMMMMM     MMMM    .MMMMMMMMMMMM.    MMMM?ZMMMMMM
+MMM               MMMMMMMM.   ?MMMMMM     MMMM     MMMMMMMMMM ,:MMMMMM?    MMMMM
+MMM           ..MMMMMMMMMM    =MMMMMM     MMMM     M$ MM$M7M $MOM MMMM     ?MMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .+Z: M   :M M  MM   ?MMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using Harmony;
+using UnityEngine.SceneManagement;
+
+namespace KK_StudioReflectFKFix 
+{
+    [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class KK_StudioReflectFKFix : BaseUnityPlugin
+    {
+        internal const string PLUGIN_NAME = "Studio Reflect FK Fix";
+        internal const string GUID = "com.jim60105.kk.studioreflectfkfix";
+        internal const string PLUGIN_VERSION = "19.05.16.0";
+
+        private bool _isInit = false;
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            string name = SceneManager.GetActiveScene().name;
+            bool flag2 = !_isInit && name == "Studio";
+            if (flag2)
+            {
+                _isInit = true;
+                HarmonyInstance harmonyInstance = HarmonyInstance.Create(GUID);
+                foreach (Type type2 in Assembly.GetExecutingAssembly().GetTypes())
+                {
+                    try
+                    {
+                        List<HarmonyMethod> harmonyMethods = HarmonyMethodExtensions.GetHarmonyMethods(type2);
+                        if (harmonyMethods != null && harmonyMethods.Count > 0)
+                        {
+                            HarmonyMethod harmonyMethod = HarmonyMethod.Merge(harmonyMethods);
+                            new PatchProcessor(harmonyInstance, type2, harmonyMethod).Patch();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Log(LogLevel.Debug, "[KK_SRFF] Exception occured when patching: " + ex.ToString());
+                    }
+                }
+                //HarmonyInstance.DEBUG = true;
+
+                Patches.InitPatch(harmonyInstance);
+                Logger.Log(LogLevel.Debug, "[KK_SRFF] Patch Insert Complete");
+            }
+        }
+
+        public void Awake()
+        {
+            SceneManager.sceneLoaded += this.OnSceneLoaded;
+        }
+    }
+}
---
+++ KK_StudioReflectFKFix/Patches.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx.Logging;
+
+using Extension;
+
+using Harmony;
+
+using Studio;
+
+using UnityEngine.UI;
+
+using Logger = BepInEx.Logger;
+
+namespace KK_StudioReflectFKFix
+{
+    class Patches
+    {
+        internal static void InitPatch(HarmonyInstance harmony)
+        {
+            harmony.Patch(typeof(MPCharCtrl).GetMethod("Awake", AccessTools.all), null, new HarmonyMethod(typeof(Patches), nameof(AwakePostfix), null), null);
+            //harmony.Patch(typeof(MPCharCtrl).GetMethod("Awake", AccessTools.all), null, null, new HarmonyMethod(typeof(Patches), nameof(AwakeTranspiler), null));
+        }
+
+        private static void AwakePostfix(MPCharCtrl __instance)
+        {
+            ((Button)__instance.GetPrivate("ikInfo").GetPrivate("buttonReflectFK")).onClick.RemoveAllListeners();
+            ((Button)__instance.GetPrivate("ikInfo").GetPrivate("buttonReflectFK")).onClick.AddListener(delegate ()
+            {
+                //__instance.CopyBoneFK((OIBoneInfo.BoneGroup)353);
+                typeof(MPCharCtrl).InvokeMember("CopyBoneFK", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.InvokeMethod, null, __instance, new object[] { (OIBoneInfo.BoneGroup)1 });
+            });
+            Logger.Log(LogLevel.Debug, "[KK_SRFF] Copy Function Rewrite");
+        }
+
+        //private static IEnumerable<CodeInstruction> AwakeTranspiler(IEnumerable<CodeInstruction> instructions)
+        //{
+        //    var codes = new List<CodeInstruction>(instructions);
+        //    for (int i = 0; i < codes.Count; i++)
+        //    {
+        //        Logger.Log(LogLevel.Debug, $"[KK_SRFF] {codes[i].opcode.ToString()}");
+        //        if (codes[i].opcode == OpCodes.Ldc_I4)
+        //        {
+        //            codes[i].operand = "1";
+        //            break;
+        //        }
+        //    }
+        //    Logger.Log(LogLevel.Debug, "[KK_SRFF] Copy Function Rewrite");
+        //    return codes.AsEnumerable();
+        //}
+
+    }
+}
