29a8c71c4427e4dfd7234597592c23baba4f0004
陳鈞 <jim60105@gmail.com>
2019-06-26 17:51:13 +0800

    v19.06.26.0 建立文字插件Studio Text Plugin

---
+++ KK_StudioTextPlugin/KK_StudioTextPlugin.cs
@@ -0,0 +1,132 @@
+﻿/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMM               MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM    M7    MZ    MMO    MMMMM
+MMM               MMMMMMMMMMMMM   MMM     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMM               MMMMMMMMMM       ?M     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMM8     MMMMMMMM       ~MMM.    MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMM        MMM                 M    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM.         ZMMMMMM     MMMM     MMMMMMMMMMMMZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM      .   ZMMMMMM     MMMM     MMMMMMMMMMMM?    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    $MMMMMM     MMMM     MMMMMMMMMMMM?    MM8    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    7MMMMMM     MMMM     MMMMMMMMMMMMI    MM8    MMMMM
+MMM               MMMMMMMM    7MMMMMM     MMMM    .MMMMMMMMMMMM.    MMMM?ZMMMMMM
+MMM               MMMMMMMM.   ?MMMMMM     MMMM     MMMMMMMMMM ,:MMMMMM?    MMMMM
+MMM           ..MMMMMMMMMM    =MMMMMM     MMMM     M$ MM$M7M $MOM MMMM     ?MMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .+Z: M   :M M  MM   ?MMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using Extension;
+using Harmony;
+using Studio;
+using UnityEngine;
+using UnityEngine.UI;
+using Logger = BepInEx.Logger;
+
+namespace KK_StudioTextPlugin {
+    [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("CharaStudio")]
+    public class KK_StudioTextPlugin : BaseUnityPlugin {
+        internal const string PLUGIN_NAME = "Studio Text Plugin";
+        internal const string GUID = "com.jim60105.kk.studiotextplugin";
+        internal const string PLUGIN_VERSION = "19.06.26.0";
+
+        public void Awake() {
+            HarmonyInstance.Create(GUID).PatchAll(typeof(Patches));
+            Patches.Start();
+        }
+    }
+
+    class Patches {
+        private static Material font3DMaterial;
+        private static bool creatingTextObj = false;
+        private static readonly string displayPrefix = "-Text Plugin:";
+        public static void Start() {
+            byte[] ba;
+            using (Stream resFilestream = Assembly.GetExecutingAssembly().GetManifestResourceStream("KK_StudioTextPlugin.Resources.text.assetbundle")) {
+                ba = new byte[resFilestream.Length];
+                resFilestream.Read(ba, 0, ba.Length);
+            }
+            if (AssetBundle.LoadFromMemory(ba) is AssetBundle assetBundle) {
+                font3DMaterial = assetBundle.LoadAsset<Material>("Font3DMaterial");
+                font3DMaterial.color = Color.white;
+            } else {
+                Logger.Log(LogLevel.Error, "[KK_STP] Load assetBundle faild");
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ItemCategoryList), "InitList")]
+        public static void InitListPostfix(int _group, ItemCategoryList __instance) {
+            if (_group == 9) {
+                GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>((GameObject)__instance.GetField("objectPrefab"));
+                if (!gameObject.activeSelf) {
+                    gameObject.SetActive(true);
+                }
+                gameObject.transform.SetParent((Transform)__instance.GetField("transformRoot"), false);
+                ListNode component = gameObject.GetComponent<ListNode>();
+                component.AddActionToButton(delegate {
+                    //創建文字
+                    creatingTextObj = true;
+                    Singleton<Studio.Studio>.Instance.AddFolder();
+                    creatingTextObj = false;
+                });
+                component.text = "文字Text";
+                ((Dictionary<int, Image>)__instance.GetField("dicNode")).Add(60105, gameObject.GetComponent<Image>());
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(AddObjectFolder), "Load", new Type[] { typeof(OIFolderInfo), typeof(ObjectCtrlInfo), typeof(TreeNodeObject), typeof(bool), typeof(int) })]
+        public static void LoadPostfix(ref OCIFolder __result, OIFolderInfo _info) {
+            if (creatingTextObj || __result.name.IndexOf(displayPrefix) >= 0) {
+                __result.name = creatingTextObj ? displayPrefix+"New Text" : _info.name;
+                MakeTextObj(__result.objectItem, creatingTextObj ? "New Text" : _info.name.Replace(displayPrefix, ""));
+                _info.changeAmount.OnChange();
+            }
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(MPFolderCtrl), "OnEndEditName")]
+        public static bool OnEditNamePrefix(MPFolderCtrl __instance, string _value) {
+            if (__instance.ociFolder.objectItem.GetComponents<TextMesh>().Length != 0) {
+                __instance.ociFolder.name = displayPrefix + _value;
+                __instance.ociFolder.objectItem.GetComponent<TextMesh>().text = _value;
+                Logger.Log(LogLevel.Info, "[KK_STP] Edit Text: " + _value);
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(MPFolderCtrl), "UpdateInfo")]
+        public static void UpdateInfoPostfix(MPFolderCtrl __instance) {
+            if (__instance.ociFolder == null) {
+                return;
+            }
+            __instance.SetField("isUpdateInfo", true);
+            InputField input = (InputField)__instance.GetField("inputName");
+            input.text = __instance.ociFolder.name.Replace(displayPrefix, "");
+            __instance.SetField("inputName", input);
+            __instance.SetField("isUpdateInfo", false);
+        }
+
+        public static void MakeTextObj(GameObject parentGO, string text) {
+            parentGO.layer = 10;
+            TextMesh t = parentGO.AddComponent<TextMesh>();
+            t.fontSize = 200;
+            t.anchor = TextAnchor.MiddleCenter;
+            t.characterSize = 0.01f;
+            t.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            t.font = Font.CreateDynamicFontFromOSFont("MS Gothic", 200);
+            parentGO.GetComponentInChildren<MeshRenderer>().material = font3DMaterial;
+            parentGO.GetComponentInChildren<MeshRenderer>().material.SetTexture("_MainTex", t.font.material.mainTexture);
+            parentGO.GetComponentInChildren<MeshRenderer>().material.EnableKeyword("_NORMALMAP");
+            t.text = text;
+            Logger.Log(LogLevel.Info, "[KK_STP] Create Text");
+        }
+    }
+}
---
+++ KK_StudioTextPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊是由下列的屬性集控制。
+// 變更這些屬性的值即可修改組件的相關
+// 資訊。
+[assembly: AssemblyTitle(KK_StudioTextPlugin.KK_StudioTextPlugin.PLUGIN_NAME)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(KK_StudioTextPlugin.KK_StudioTextPlugin.PLUGIN_NAME)]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設定為 false 會使得這個組件中的類型 
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中 
+// 的類型，請在該類型上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("992E82B8-3015-45FA-80D9-EB41CDE013FB")]
+
+// 組件的版本資訊由下列四個值所組成: 
+//
+//      主要版本
+//      次要版本 
+//      組建編號
+//      修訂編號
+//
+[assembly: AssemblyVersion(KK_StudioTextPlugin.KK_StudioTextPlugin.PLUGIN_VERSION)]
+[assembly: AssemblyFileVersion(KK_StudioTextPlugin.KK_StudioTextPlugin.PLUGIN_VERSION)]
