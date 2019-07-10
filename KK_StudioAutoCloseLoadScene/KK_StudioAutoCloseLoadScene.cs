fbc41a0d2633dc694b4c71f4271e854e38289c99
陳鈞 <jim60105@gmail.com>
2019-07-10 18:03:28 +0800

    v19.07.10.0 新增: Studio Auto Close Load Scene

---
+++ KK_StudioAutoCloseLoadScene/KK_StudioAutoCloseLoadScene.cs
@@ -0,0 +1,54 @@
+﻿/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMM               MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM    M7    MZ    MMO    MMMMM
+MMM               MMMMMMMMMMMMM   MMM     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMM               MMMMMMMMMM       ?M     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMM8     MMMMMMMM       ~MMM.    MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMM        MMM                 M    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM.         ZMMMMMM     MMMM     MMMMMMMMMMMMZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM      .   ZMMMMMM     MMMM     MMMMMMMMMMMM?    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    $MMMMMM     MMMM     MMMMMMMMMMMM?    MM8    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    7MMMMMM     MMMM     MMMMMMMMMMMMI    MM8    MMMMM
+MMM               MMMMMMMM    7MMMMMM     MMMM    .MMMMMMMMMMMM.    MMMM?ZMMMMMM
+MMM               MMMMMMMM.   ?MMMMMM     MMMM     MMMMMMMMMM ,:MMMMMM?    MMMMM
+MMM           ..MMMMMMMMMM    =MMMMMM     MMMM     M$ MM$M7M $MOM MMMM     ?MMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .+Z: M   :M M  MM   ?MMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+using BepInEx;
+using BepInEx.Logging;
+using Extension;
+using Harmony;
+using Studio;
+using Logger = BepInEx.Logger;
+
+namespace KK_StudioAutoCloseLoadScene {
+    [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("CharaStudio")]
+    public class KK_StudioAutoCloseLoadScene : BaseUnityPlugin {
+        internal const string PLUGIN_NAME = "Studio Auto Close Load Scene";
+        internal const string GUID = "com.jim60105.kk.studioautocloseloadscene";
+        internal const string PLUGIN_VERSION = "19.07.10.0";
+
+        public void Awake() {
+            BepInEx.Config.ReloadConfig();
+            if (string.Equals(BepInEx.Config.GetEntry("enabled", "True", PLUGIN_NAME), "True")) {
+                HarmonyInstance.Create(GUID).PatchAll(typeof(Patches));
+            }
+        }
+    }
+
+    class Patches {
+        [HarmonyPostfix, HarmonyPatch(typeof(SceneLoadScene), "OnClickLoad")]
+        public static void OnClickLoadPostfix(SceneLoadScene __instance) => Close(__instance);
+        [HarmonyPostfix, HarmonyPatch(typeof(SceneLoadScene), "OnClickImport")]
+        public static void OnClickImportPostfix(SceneLoadScene __instance) => Close(__instance);
+
+        private static void Close(SceneLoadScene __instance) {
+            __instance.Invoke("OnClickClose");
+            Logger.Log(LogLevel.Debug, "[KK_SACLS] Auto close load scene window");
+        }
+    }
+}
---
+++ KK_StudioAutoCloseLoadScene/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊是由下列的屬性集控制。
+// 變更這些屬性的值即可修改組件的相關
+// 資訊。
+[assembly: AssemblyTitle(KK_StudioAutoCloseLoadScene.KK_StudioAutoCloseLoadScene.PLUGIN_NAME)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(KK_StudioAutoCloseLoadScene.KK_StudioAutoCloseLoadScene.PLUGIN_NAME)]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設定為 false 會使得這個組件中的類型 
+// 對 COM 元件而言為不可見。如果您需要從 COM 存取這個組件中 
+// 的類型，請在該類型上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("3E02972B-3D85-4E1B-A76C-878DCD103A68")]
+
+
+// 組件的版本資訊由下列四個值所組成: 
+//
+//      主要版本
+//      次要版本 
+//      組建編號
+//      修訂編號
+//
+[assembly: AssemblyVersion(KK_StudioAutoCloseLoadScene.KK_StudioAutoCloseLoadScene.PLUGIN_VERSION)]
+[assembly: AssemblyFileVersion(KK_StudioAutoCloseLoadScene.KK_StudioAutoCloseLoadScene.PLUGIN_VERSION)]
