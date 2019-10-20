e0d1ab8101cfa37805f0cf9efb6768c5eb4ffb05
陳鈞 <jim60105@gmail.com>
2019-10-21 02:33:05 +0800

    v19.10.21.0 KK_SCLO更改一處日文文字

--- KK_StudioCoordinateLoadOption/KK_StudioCoordinateLoadOption.cs
+++ KK_StudioCoordinateLoadOption/KK_StudioCoordinateLoadOption.cs
@@ -40,7 +40,7 @@ namespace KK_StudioCoordinateLoadOption {
     public class KK_StudioCoordinateLoadOption : BaseUnityPlugin {
         internal const string PLUGIN_NAME = "Studio Coordinate Load Option";
         internal const string GUID = "com.jim60105.kk.studiocoordinateloadoption";
-        internal const string PLUGIN_VERSION = "19.10.11.3";
+        internal const string PLUGIN_VERSION = "19.10.21.0";
 
         public void Awake() {
             UIUtility.Init();
