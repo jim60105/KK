92912b824e1a03472465cc1c085c947dc59f1ed6
陳鈞 <jim60105@gmail.com>
2019-12-19 03:19:05 +0800

    v19.12.19.0 KK_PLT插件清單工具: 新增: Excel XML輸出

--- KK_PluginListTool/KK_PluginListTool.cs
+++ KK_PluginListTool/KK_PluginListTool.cs
@@ -27,17 +27,19 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Xml.Linq;
 
 namespace KK_PluginListTool {
     [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class KK_PluginListTool : BaseUnityPlugin {
         internal const string PLUGIN_NAME = "Plugin List Tool";
         internal const string GUID = "com.jim60105.kk.pluginlisttool";
-        internal const string PLUGIN_VERSION = "19.12.18.0";
+        internal const string PLUGIN_VERSION = "19.12.19.0";
         internal static new ManualLogSource Logger;
 
         bool init = false;
         internal static List<string> strList = new List<string>();
+        internal static List<Plugin> pluginList = new List<Plugin>();
         public void LateUpdate() {
             if (!init) {
                 init = true;
@@ -63,20 +65,30 @@ namespace KK_PluginListTool {
                 //BepPlugin
                 Logger.LogDebug($">>Get {BepInEx.Bootstrap.Chainloader.PluginInfos.Count} BepInEx Plugins.");
                 foreach (var kv in BepInEx.Bootstrap.Chainloader.PluginInfos) {
-                    AddPlugin(
+                    AddJsonString(
                         kv.Value.Metadata.GUID,
                         kv.Value.Metadata.Name,
                         kv.Value.Metadata.Version.ToString()
                     );
+                    pluginList.Add(new Plugin(
+                        kv.Value.Metadata.GUID,
+                        kv.Value.Metadata.Name,
+                        kv.Value.Metadata.Version.ToString()
+                    ));
                 }
 
-                FileLog.logPath = Path.Combine(Path.GetDirectoryName(base.Info.Location), Path.GetFileNameWithoutExtension(Paths.ExecutablePath)) + "_LoadedPluginList.json";
+                FileLog.logPath = Path.Combine(Path.GetDirectoryName(base.Info.Location), Path.GetFileNameWithoutExtension(Paths.ExecutablePath)) + "_LoadedPluginList_JSON.json";
                 if (File.Exists(FileLog.logPath)) {
                     File.Delete(FileLog.logPath);
                 }
                 FileLog.Log(JsonHelper.FormatJson($"[{strList.Join(delimiter: ",")}]"));
 
-                Logger.LogInfo($"Logged Plugin Info into: {FileLog.logPath}");
+                Logger.LogInfo($"Logged JSON into: {FileLog.logPath}");
+
+                string xmlPath = Path.Combine(Path.GetDirectoryName(base.Info.Location), Path.GetFileNameWithoutExtension(Paths.ExecutablePath)) + "_LoadedPluginList_ExcelXML.xml";
+                XDocument xdoc = pluginList.Select(x => (object)x).ToExcelXml();
+                xdoc.Save(xmlPath);
+                Logger.LogInfo($"Logged Excel XML into: {xmlPath}");
             }
         }
 
@@ -85,16 +97,21 @@ namespace KK_PluginListTool {
                 List<T> newList = new List<T>(iEnumerable);
                 Logger.LogDebug($">>Get {newList.Count} IPA Plugins.");
                 foreach (var l in newList) {
-                    AddPlugin(
+                    AddJsonString(
                         "IPA." + ((string)l.GetProperty("Name")).Replace(' ', '.'),   //IPlugin結構內沒有GUID，姑且拼一個
                         (string)l.GetProperty("Name"),
                         (string)l.GetProperty("Version")
                     );
+                    pluginList.Add(new Plugin(
+                        "IPA." + ((string)l.GetProperty("Name")).Replace(' ', '.'),   //IPlugin結構內沒有GUID，姑且拼一個
+                        (string)l.GetProperty("Name"),
+                        (string)l.GetProperty("Version")
+                    ));
                 }
             }
         }
 
-        public static void AddPlugin(string guid, string name, string version) {
+        public static void AddJsonString(string guid, string name, string version) {
             try {
                 //Log to Console
                 Logger.LogDebug($"{name} v{version}");
@@ -111,6 +128,23 @@ namespace KK_PluginListTool {
             };
         }
     }
+
+    public class Plugin {
+        private string guid;
+        private string name;
+        private string version;
+
+        public string GUID { get => guid; set => guid = value; }
+        public string Name { get => name; set => name = value; }
+        public string Version { get => version; set => version = value; }
+
+        public Plugin(string guid, string name, string version) {
+            GUID = guid;
+            Name = name;
+            Version = version;
+        }
+    }
+
 
     #region JSONTool 
     //JSON formatter in C#  - Stack Overflow
@@ -175,6 +209,212 @@ namespace KK_PluginListTool {
             foreach (var i in ie) {
                 action(i);
             }
+        }
+    }
+    #endregion
+
+    #region ExcelXmlTool
+    public static class ExcelExportExtensions {
+        public static XDocument ToExcelXml(this IEnumerable<object> rows) {
+            return rows.ToExcelXml("Sheet1");
+        }
+
+        public static XDocument ToExcelXml(this IEnumerable<object> rows, string sheetName) {
+            sheetName = sheetName.Replace("/", "-");
+            sheetName = sheetName.Replace("\\", "-");
+
+            XNamespace mainNamespace = "urn:schemas-microsoft-com:office:spreadsheet";
+            XNamespace o = "urn:schemas-microsoft-com:office:office";
+            XNamespace x = "urn:schemas-microsoft-com:office:excel";
+            XNamespace ss = "urn:schemas-microsoft-com:office:spreadsheet";
+            XNamespace html = "http://www.w3.org/TR/REC-html40";
+
+            XDocument xdoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                new XProcessingInstruction("mso-application", "progid=\"Excel.Sheet\""));
+
+            var headerRow = from p in rows.First().GetType().GetProperties()
+                            select new XElement(mainNamespace + "Cell",
+                                new XElement(mainNamespace + "Data",
+                                    new XAttribute(ss + "Type", "String"), p.Name)); //Generate header using reflection
+
+            XElement workbook = new XElement(mainNamespace + "Workbook",
+                new XAttribute(XNamespace.Xmlns + "html", html),
+                new XAttribute(XName.Get("ss", "http://www.w3.org/2000/xmlns/"), ss),
+                new XAttribute(XName.Get("o", "http://www.w3.org/2000/xmlns/"), o),
+                new XAttribute(XName.Get("x", "http://www.w3.org/2000/xmlns/"), x),
+                new XAttribute(XName.Get("xmlns", ""), mainNamespace),
+                new XElement(o + "DocumentProperties",
+                        new XAttribute(XName.Get("xmlns", ""), o),
+                        new XElement(o + "Author", KK_PluginListTool.GUID),
+                        new XElement(o + "LastAuthor", KK_PluginListTool.GUID),
+                        new XElement(o + "Created", DateTime.Now.ToString())
+                    ), //end document properties
+                new XElement(x + "ExcelWorkbook",
+                        new XAttribute(XName.Get("xmlns", ""), x),
+                        new XElement(x + "WindowHeight", 12750),
+                        new XElement(x + "WindowWidth", 24855),
+                        new XElement(x + "WindowTopX", 240),
+                        new XElement(x + "WindowTopY", 75),
+                        new XElement(x + "ProtectStructure", "False"),
+                        new XElement(x + "ProtectWindows", "False")
+                    ), //end ExcelWorkbook
+                new XElement(mainNamespace + "Styles",
+                        new XElement(mainNamespace + "Style",
+                            new XAttribute(ss + "ID", "Default"),
+                            new XAttribute(ss + "Name", "Normal"),
+                            new XElement(mainNamespace + "Alignment",
+                                new XAttribute(ss + "Vertical", "Bottom")
+                            ),
+                            new XElement(mainNamespace + "Borders"),
+                            new XElement(mainNamespace + "Font",
+                                new XAttribute(ss + "FontName", "Calibri"),
+                                new XAttribute(x + "Family", "Swiss"),
+                                new XAttribute(ss + "Size", "11"),
+                                new XAttribute(ss + "Color", "#000000")
+                            ),
+                            new XElement(mainNamespace + "Interior"),
+                            new XElement(mainNamespace + "NumberFormat"),
+                            new XElement(mainNamespace + "Protection")
+                        ),
+                        new XElement(mainNamespace + "Style",
+                            new XAttribute(ss + "ID", "Header"),
+                            new XElement(mainNamespace + "Font",
+                                new XAttribute(ss + "FontName", "Calibri"),
+                                new XAttribute(x + "Family", "Swiss"),
+                                new XAttribute(ss + "Size", "11"),
+                                new XAttribute(ss + "Color", "#000000"),
+                                new XAttribute(ss + "Bold", "1")
+                            )
+                        )
+                    ), // close styles
+                    new XElement(mainNamespace + "Worksheet",
+                        new XAttribute(ss + "Name", sheetName /* Sheet name */),
+                        new XElement(mainNamespace + "Table",
+                            new XAttribute(ss + "ExpandedColumnCount", headerRow.Count()),
+                            new XAttribute(ss + "ExpandedRowCount", rows.Count() + 1),
+                            new XAttribute(x + "FullColumns", 1),
+                            new XAttribute(x + "FullRows", 1),
+                            new XAttribute(ss + "DefaultRowHeight", 15),
+                            new XElement(mainNamespace + "Column",
+                                new XAttribute(ss + "Width", 81)
+                            ),
+                            new XElement(mainNamespace + "Row", new XAttribute(ss + "StyleID", "Header"), headerRow),
+                            from contentRow in rows
+                            select new XElement(mainNamespace + "Row",
+                                new XAttribute(ss + "StyleID", "Default"),
+                                    from p in contentRow.GetType().GetProperties()
+                                    select new XElement(mainNamespace + "Cell",
+                                         new XElement(mainNamespace + "Data", new XAttribute(ss + "Type", "String"), p.GetValue(contentRow, null))) /* Build cells using reflection */ )
+                        ), //close table
+                        new XElement(x + "WorksheetOptions",
+                            new XAttribute(XName.Get("xmlns", ""), x),
+                            new XElement(x + "PageSetup",
+                                new XElement(x + "Header",
+                                    new XAttribute(x + "Margin", "0.3")
+                                ),
+                                new XElement(x + "Footer",
+                                    new XAttribute(x + "Margin", "0.3")
+                                ),
+                                new XElement(x + "PageMargins",
+                                    new XAttribute(x + "Bottom", "0.75"),
+                                    new XAttribute(x + "Left", "0.7"),
+                                    new XAttribute(x + "Right", "0.7"),
+                                    new XAttribute(x + "Top", "0.75")
+                                )
+                            ),
+                            new XElement(x + "Print",
+                                new XElement(x + "ValidPrinterInfo"),
+                                new XElement(x + "HorizontalResolution", 600),
+                                new XElement(x + "VerticalResolution", 600)
+                            ),
+                            new XElement(x + "Selected"),
+                            new XElement(x + "Panes",
+                                new XElement(x + "Pane",
+                                    new XElement(x + "Number", 3),
+                                    new XElement(x + "ActiveRow", 1),
+                                    new XElement(x + "ActiveCol", 0)
+                                )
+                            ),
+                            new XElement(x + "ProtectObjects", "False"),
+                            new XElement(x + "ProtectScenarios", "False")
+                        ) // close worksheet options
+                    ) // close Worksheet
+                );
+
+            xdoc.Add(workbook);
+
+            return xdoc;
+        }
+
+        public static XElement ToExcelXmlWorksheet(this IEnumerable<object> rows, string sheetName) {
+            sheetName = sheetName.Replace("/", "-");
+            sheetName = sheetName.Replace("\\", "-");
+
+            XNamespace mainNamespace = "urn:schemas-microsoft-com:office:spreadsheet";
+            XNamespace o = "urn:schemas-microsoft-com:office:office";
+            XNamespace x = "urn:schemas-microsoft-com:office:excel";
+            XNamespace ss = "urn:schemas-microsoft-com:office:spreadsheet";
+            XNamespace html = "http://www.w3.org/TR/REC-html40";
+
+            var headerRow = from p in rows.First().GetType().GetProperties()
+                            select new XElement(mainNamespace + "Cell",
+                                new XElement(mainNamespace + "Data",
+                                    new XAttribute(ss + "Type", "String"), p.Name)); //Generate header using reflection
+
+            XElement worksheet = new XElement(mainNamespace + "Worksheet",
+                    new XAttribute(ss + "Name", sheetName /* Sheet name */),
+                    new XElement(mainNamespace + "Table",
+                        new XAttribute(ss + "ExpandedColumnCount", headerRow.Count()),
+                        new XAttribute(ss + "ExpandedRowCount", rows.Count() + 1),
+                        new XAttribute(x + "FullColumns", 1),
+                        new XAttribute(x + "FullRows", 1),
+                        new XAttribute(ss + "DefaultRowHeight", 15),
+                        new XElement(mainNamespace + "Column",
+                            new XAttribute(ss + "Width", 81)
+                        ),
+                        new XElement(mainNamespace + "Row", new XAttribute(ss + "StyleID", "Header"), headerRow),
+                        from contentRow in rows
+                        select new XElement(mainNamespace + "Row",
+                            new XAttribute(ss + "StyleID", "Default"),
+                                from p in contentRow.GetType().GetProperties()
+                                select new XElement(mainNamespace + "Cell",
+                                        new XElement(mainNamespace + "Data", new XAttribute(ss + "Type", "String"), p.GetValue(contentRow, null))) /* Build cells using reflection */ )
+                    ), //close table
+                    new XElement(x + "WorksheetOptions",
+                        new XAttribute(XName.Get("xmlns", ""), x),
+                        new XElement(x + "PageSetup",
+                            new XElement(x + "Header",
+                                new XAttribute(x + "Margin", "0.3")
+                            ),
+                            new XElement(x + "Footer",
+                                new XAttribute(x + "Margin", "0.3")
+                            ),
+                            new XElement(x + "PageMargins",
+                                new XAttribute(x + "Bottom", "0.75"),
+                                new XAttribute(x + "Left", "0.7"),
+                                new XAttribute(x + "Right", "0.7"),
+                                new XAttribute(x + "Top", "0.75")
+                            )
+                        ),
+                        new XElement(x + "Print",
+                            new XElement(x + "ValidPrinterInfo"),
+                            new XElement(x + "HorizontalResolution", 600),
+                            new XElement(x + "VerticalResolution", 600)
+                        ),
+                        new XElement(x + "Selected"),
+                        new XElement(x + "Panes",
+                            new XElement(x + "Pane",
+                                new XElement(x + "Number", 3),
+                                new XElement(x + "ActiveRow", 1),
+                                new XElement(x + "ActiveCol", 0)
+                            )
+                        ),
+                        new XElement(x + "ProtectObjects", "False"),
+                        new XElement(x + "ProtectScenarios", "False")
+                    ) // close worksheet options
+                ); // close Worksheet
+
+            return worksheet;
         }
     }
     #endregion
