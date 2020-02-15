30f9fd24384690796b97ee133e3b3cc0a405b97c
陳鈞 <jim60105@gmail.com>
2020-02-15 10:35:37 +0800

    v20.02.15.0 PNGCSM 增加: 改變Maker圖片存檔列數功能

--- PNGCaptureSizeModifier/PNGCaptureSizeModifier.cs
+++ PNGCaptureSizeModifier/PNGCaptureSizeModifier.cs
@@ -21,27 +21,33 @@ using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Harmony;
 using BepInEx.Logging;
+using Extension;
 using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace PNGCaptureSizeModifier {
     [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class PNGCaptureSizeModifier : BaseUnityPlugin {
         internal const string PLUGIN_NAME = "PNG Capture Size Modifier";
         internal const string GUID = "com.jim60105.kk.pngcapturesizemodifier";
-        internal const string PLUGIN_VERSION = "20.02.14.0";
-        internal const string PLUGIN_RELEASE_VERSION = "1.0.0";
+        internal const string PLUGIN_VERSION = "20.02.15.0";
+        internal const string PLUGIN_RELEASE_VERSION = "1.1.0";
 
         public static ConfigEntry<float> TimesOfMaker { get; private set; }
         public static ConfigEntry<float> TimesOfStudio { get; private set; }
+        public static ConfigEntry<int> PNGColumnCount { get; private set; }
         internal static new ManualLogSource Logger;
         public void Awake() {
             Logger = base.Logger;
             TimesOfMaker = Config.Bind<float>("Config", "Times of multiplication (Maker)", 3.0f, "The game needs to be restarted for changes to take effect.");
             TimesOfStudio = Config.Bind<float>("Config", "Times of multiplication (Studio)", 5.0f, "The game needs to be restarted for changes to take effect.");
+            PNGColumnCount = Config.Bind<int>("Config", "Number of PNG rows in File List View", 3, "Must be a natural number");
             HarmonyWrapper.PatchAll(typeof(Patches));
         }
     }
@@ -70,6 +76,38 @@ namespace PNGCaptureSizeModifier {
                 }
             }
             return codes.AsEnumerable();
+        }
+
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaCustom.CustomFileListCtrl), "Update")]
+        public static void UpdatePostFix(ChaCustom.CustomFileListCtrl __instance) => ChangeRowCount((ChaCustom.CustomFileWindow)__instance.GetField("cfWindow"));
+
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaCustom.CustomCoordinateFile), "Start")]
+        public static void StartPostFix(ChaCustom.CustomCoordinateFile __instance) => ChangeRowCount((ChaCustom.CustomFileWindow)__instance.GetField("fileWindow"));
+
+        public static void ChangeRowCount(ChaCustom.CustomFileWindow window) {
+            GridLayoutGroup component = window.gameObject.GetComponentInChildren<GridLayoutGroup>();
+            int count = PNGCaptureSizeModifier.PNGColumnCount.Value;
+            if (component.constraintCount != count) {
+                if (count == 0) {
+                    PNGCaptureSizeModifier.PNGColumnCount.Value = (int)PNGCaptureSizeModifier.PNGColumnCount.DefaultValue;
+                    return;
+                }
+                RectTransform rect = component.transform as RectTransform;
+                component.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+                component.constraintCount = count;
+                float width = (rect.rect.width - (9 * (count - 1)) - 20) / count;
+                component.cellSize = new Vector2(width, width / component.cellSize.x * component.cellSize.y);
+                UpdateLayout(component.transform as RectTransform);
+            }
+        }
+
+        public static IEnumerator UpdateLayout(RectTransform rect) {
+            //在某些奇妙的狀況會需要多呼叫兩次
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+            yield return new WaitForEndOfFrame();
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+            yield return new WaitForEndOfFrame();
         }
     }
 }
