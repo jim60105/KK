9dc98d662f63505ec4120aca0c3934c271818ab2
陳鈞 <jim60105@gmail.com>
2020-03-18 01:36:05 +0800

    v20.03.18.0 KK_SCLLTC 新增: 1. 完善存檔功能 2. 正確的四元數計算

--- KK_StudioCharaLightLinkedToCamera/KK_StudioCharaLightLinkedToCamera.cs
+++ KK_StudioCharaLightLinkedToCamera/KK_StudioCharaLightLinkedToCamera.cs
@@ -18,6 +18,7 @@ MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 */
 
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Harmony;
 using BepInEx.Logging;
 using ExtensibleSaveFormat;
@@ -34,13 +35,21 @@ namespace KK_StudioCharaLightLinkedToCamera {
     public class KK_StudioCharaLightLinkedToCamera : BaseUnityPlugin {
         internal const string PLUGIN_NAME = "Studio Chara Light Linked To Camera";
         internal const string GUID = "com.jim60105.kk.studiocharalightlinkedtocamera";
-        internal const string PLUGIN_VERSION = "20.03.16.0";
-        internal const string PLUGIN_RELEASE_VERSION = "1.1.1.2";
+        internal const string PLUGIN_VERSION = "20.03.18.0";
+        internal const string PLUGIN_RELEASE_VERSION = "1.1.2";
+
+        /// <summary>
+        /// True: 將Quaternion換為俯仰角以填入UI才能儲存，但是此方法在垂直往上/往下看時光源會偏移。
+        /// False: 以計算出的Quaternion值旋轉transRoot是為直接操作，這樣在任何視角都會是正確光源，但是無法重整UI儲存數值。
+        /// </summary>
+        public static ConfigEntry<bool> RefreshUI { get; private set; }
 
         internal static new ManualLogSource Logger;
         public void Awake() {
             Logger = base.Logger;
             HarmonyWrapper.PatchAll(typeof(Patches));
+
+            RefreshUI = Config.Bind<bool>("Config", "Refresh UI", true, "Due to KK's character light design, if the light is forced to lock properly, the UI cannot be refreshed and stored in vanilla SceneData. Use this feature at your own risk.");
         }
     }
 
@@ -48,31 +57,13 @@ namespace KK_StudioCharaLightLinkedToCamera {
         private static readonly ManualLogSource Logger = KK_StudioCharaLightLinkedToCamera.Logger;
         public static bool locked = false;
         private static readonly object studioLightCalc = Singleton<Studio.Studio>.Instance.cameraLightCtrl.GetField("lightChara");
+        private static Transform transRoot;
         private static Quaternion angleDiff = Quaternion.Euler(0, 0, 0);
-
-        public static void RegisterSaveEvent() {
-            ExtendedSave.SceneBeingSaved += path => {
-                ExtendedSave.SetSceneExtendedDataById(KK_StudioCharaLightLinkedToCamera.GUID, new PluginData() {
-                    data = new System.Collections.Generic.Dictionary<string, object> {
-                        { "locked", locked ? "true" : "false" }
-                    },
-                    version = 1
-                });
-                Logger.LogDebug("Scene Saved");
-            };
-            ExtendedSave.SceneBeingLoaded += path => {
-                PluginData pd = ExtendedSave.GetSceneExtendedDataById(KK_StudioCharaLightLinkedToCamera.GUID);
-                if (null != pd && pd.version == 1 && pd.data.TryGetValue("locked", out object l) && l is string boolstring) {
-                    ToggleLocked(boolstring == "true");
-                } else {
-                    ToggleLocked(false);
-                }
-            };
-        }
 
         #region View
         static private Selectable[] interactableGroup;
         static internal GameObject LockBtn;
+
         [HarmonyPostfix, HarmonyPatch(typeof(Studio.CameraLightCtrl), "Init")]
         public static void InitPostfix() {
             if (null != GameObject.Find("StudioScene/Canvas Main Menu/04_System/03_Light/Chara Light Lock Btn")) {
@@ -100,9 +91,9 @@ namespace KK_StudioCharaLightLinkedToCamera {
 
             LockBtn.GetComponent<Button>().onClick.AddListener(delegate { ToggleLocked(); });
 
-            (studioLightCalc.GetField("transRoot") as Transform).localRotation = Quaternion.Euler(0, 0, 0);
+            transRoot = (studioLightCalc.GetField("transRoot") as Transform);
+            //transRoot.localRotation = Quaternion.Euler(0, 0, 0);
             RegisterSaveEvent();
-            //Logger.LogMessage("Draw Button Finish");
         }
 
         public static void ToggleLocked(bool? b = null) {
@@ -127,27 +118,74 @@ namespace KK_StudioCharaLightLinkedToCamera {
         }
         #endregion
 
-        [HarmonyPostfix, HarmonyPatch(typeof(Studio.CameraControl), "CameraUpdate")]
-        public static void CameraUpdatePostfix(Studio.CameraControl __instance) {
-            if (!locked) return;
+        #region SaveAndLoad
+        public static void RegisterSaveEvent() {
+            ExtendedSave.SceneBeingSaved += path => {
+                ExtendedSave.SetSceneExtendedDataById(KK_StudioCharaLightLinkedToCamera.GUID, new PluginData() {
+                    data = new System.Collections.Generic.Dictionary<string, object> {
+                        { "locked", locked ? "true" : "false" },
+                        { "angleDiff", new System.Collections.Generic.Dictionary<string,float>{
+                            {"x",angleDiff.x },
+                            {"y",angleDiff.y },
+                            {"z",angleDiff.z },
+                            {"w",angleDiff.w }
+                        }}
+                    },
+                    version = 2
+                });
+                Logger.LogDebug("Scene Saved");
+            };
+            ExtendedSave.SceneBeingLoaded += path => {
+                PluginData pd = ExtendedSave.GetSceneExtendedDataById(KK_StudioCharaLightLinkedToCamera.GUID);
+                if (null != pd && pd.version == 2 &&
+                    pd.data.TryGetValue("locked", out object l) && l is string boolstring &&
+                    pd.data.TryGetValue("angleDiff", out object obj)) {
 
-            Transform transRoot = studioLightCalc.GetField("transRoot") as Transform;
-            transRoot.localRotation = Quaternion.Euler(Singleton<Studio.CameraControl>.Instance.cameraAngle) * angleDiff;
-            //Quaternion q = Quaternion.FromToRotation(new Vector3(1,0,0),q1.eulerAngles);
-            //Studio.CameraLightCtrl.LightInfo charaLight = Singleton<Studio.Studio>.Instance.sceneInfo.charaLight;
-            //charaLight.rot[0] = (q.eulerAngles).x;
-            //charaLight.rot[1] = (q.eulerAngles).y;
-            //studioLightCalc.Invoke("OnValueChangeAxis", new object[] { q.eulerAngles.x, 0 });
-            //studioLightCalc.Invoke("OnValueChangeAxis", new object[] { q.eulerAngles.y, 1 });
-            //studioLightCalc.Invoke("UpdateUI");
-
-            //Logger.LogDebug($"LoaclRotation: {transRoot.localRotation.eulerAngles.ToString()}");
-            //Logger.LogDebug($"CameraAngle: {__instance.cameraAngle[0]}, {__instance.cameraAngle[1]} / LightAngle: {(Quaternion.Euler(Singleton<Studio.CameraControl>.Instance.cameraAngle) * angleDiff).eulerAngles.ToString()}");
+                    System.Collections.Generic.Dictionary<string, float> d = obj.ToDictionary<string, float>();
+                    ToggleLocked(boolstring == "true");
+                    angleDiff = new Quaternion(d["x"], d["y"], d["z"], d["w"]);
+                    Logger.LogDebug("Scene Load PluginData");
+                } else {
+                    ToggleLocked(false);
+                }
+            };
         }
 
         [HarmonyPrefix, HarmonyPatch(typeof(Studio.SceneLoadScene), "OnClickLoad")]
         public static void OnClickLoadPrefix() {
+            //LoadScene以前先解除Locked狀態
             ToggleLocked(false);
+        }
+
+        #endregion
+
+        [HarmonyPostfix, HarmonyPatch(typeof(Studio.CameraControl), "CameraUpdate")]
+        public static void CameraUpdatePostfix() {
+            if (!locked) return;
+            Quaternion q = Quaternion.Euler(Singleton<Studio.CameraControl>.Instance.cameraAngle) * angleDiff;
+
+            //這裡分成兩種方式操作
+            // True: 將Quaternion換為俯仰角以填入UI才能儲存，但是此方法在垂直往上/往下看時光源會偏移。
+            // False: 以計算出的Quaternion值旋轉transRoot是為直接操作，這樣在任何視角都會是正確光源，但是無法重整UI儲存數值。
+            if (KK_StudioCharaLightLinkedToCamera.RefreshUI.Value) {
+                //finding pitch_roll_yaw from Quaternions - Unity Answers
+                //https://answers.unity.com/questions/416169/finding-pitchrollyaw-from-quaternions.html
+
+                float roll = Mathf.Atan2(2 * q.y * q.w - 2 * q.x * q.z, 1 - 2 * q.y * q.y - 2 * q.z * q.z) * Mathf.Rad2Deg;
+                float pitch = Mathf.Atan2(2 * q.x * q.w - 2 * q.y * q.z, 1 - 2 * q.x * q.x - 2 * q.z * q.z) * Mathf.Rad2Deg;
+                //float yaw = Mathf.Asin(2 * x * y + 2 * z * w) * Mathf.Rad2Deg;
+
+                roll = (roll >= 0) ? roll : roll + 360;
+                pitch = (pitch >= 0) ? pitch : pitch + 360;
+
+                studioLightCalc.Invoke("OnValueChangeAxis", new object[] { pitch, 0 });
+                studioLightCalc.Invoke("OnValueChangeAxis", new object[] { roll, 1 });
+                studioLightCalc.Invoke("UpdateUI");
+                //Logger.LogDebug($" {pitch}, {roll}");
+            } else {
+                transRoot.localRotation = q;
+            }
+            //Logger.LogDebug($"CameraAngle: {__instance.cameraAngle[0]}, {__instance.cameraAngle[1]} / LightAngle: {(Quaternion.Euler(Singleton<Studio.CameraControl>.Instance.cameraAngle) * angleDiff).eulerAngles.ToString()}");
         }
     }
 }
