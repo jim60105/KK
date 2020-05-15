f73f9c8deb36f4b01fcb102dff76c8102156e843
陳鈞 <jim60105@gmail.com>
2020-05-15 17:45:04 +0800

    v20.05.15.0 KK_SSWOF First Commit

---
+++ KK_StudioSaveWorkspaceOrderFix/KK_StudioSaveWorkspaceOrderFix.cs
@@ -0,0 +1,67 @@
+﻿/*
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMM               MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM    M7    MZ    MMO    MMMMM
+MMM               MMMMMMMMMMMMM   MMM     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMM               MMMMMMMMMM       ?M     MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMM8     MMMMMMMM       ~MMM.    MMMMMMMMMM    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMM        MMM                 M    M$    MZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM.         ZMMMMMM     MMMM     MMMMMMMMMMMMZ    MMO    MMMMM
+MMMMMMMMMMMMM     MM      .   ZMMMMMM     MMMM     MMMMMMMMMMMM?    MMO    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    $MMMMMM     MMMM     MMMMMMMMMMMM?    MM8    MMMMM
+MMMMMMMMMMMMM     MMMMMMMM    7MMMMMM     MMMM     MMMMMMMMMMMMI    MM8    MMMMM
+MMM               MMMMMMMM    7MMMMMM     MMMM    .MMMMMMMMMMMM.    MMMM?ZMMMMMM
+MMM               MMMMMMMM.   ?MMMMMM     MMMM     MMMMMMMMMM ,:MMMMMM?    MMMMM
+MMM           ..MMMMMMMMMM    =MMMMMM     MMMM     M$ MM$M7M $MOM MMMM     ?MMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .+Z: M   :M M  MM   ?MMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+using BepInEx;
+using BepInEx.Harmony;
+using BepInEx.Logging;
+using Extension;
+using HarmonyLib;
+using Studio;
+using System.Collections.Generic;
+
+namespace KK_StudioSaveWorkspaceOrderFix {
+    [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInProcess("CharaStudio")]
+    public class KK_StudioSaveWorkspaceOrderFix : BaseUnityPlugin {
+        internal const string PLUGIN_NAME = "Studio Save Workspace Order Fix";
+        internal const string GUID = "com.jim60105.kk.studiosaveworkspaceorderfix";
+        internal const string PLUGIN_VERSION = "20.05.15.0";
+        internal const string PLUGIN_RELEASE_VERSION = "0.0.0";
+
+        internal static new ManualLogSource Logger;
+        public void Awake() {
+            Logger = base.Logger;
+            HarmonyWrapper.PatchAll(typeof(Patches));
+        }
+    }
+    class Patches {
+        [HarmonyPrefix, HarmonyPatch(typeof(Studio.Studio), "SaveScene")]
+        public static void SaveScenePrefix(Studio.Studio __instance) {
+            Dictionary<int, ObjectInfo> dicObject = __instance.sceneInfo.dicObject;
+            List<TreeNodeObject> treeNodeObj = __instance.treeNodeCtrl.GetField("m_TreeNodeObject") as List<TreeNodeObject>;
+            Dictionary<int, ObjectInfo> resultDicObject = new Dictionary<int, ObjectInfo>();
+
+            foreach (TreeNodeObject t in treeNodeObj) {
+                if (null == t) continue;
+
+                if (Singleton<Studio.Studio>.Instance.dicInfo.TryGetValue(t, out ObjectCtrlInfo item) &&
+                    dicObject.TryGetValue(item.objectInfo.dicKey, out ObjectInfo objInfo) &&
+                    null != objInfo &&
+                    objInfo == item.objectInfo
+                ) {
+                    resultDicObject.Add(objInfo.dicKey, objInfo);
+                }
+            }
+
+            KK_StudioSaveWorkspaceOrderFix.Logger.LogDebug($"{dicObject.Count} -> {resultDicObject.Count}");
+
+            __instance.sceneInfo.dicObject = resultDicObject;
+        }
+    }
+}
---
+++ KK_StudioSaveWorkspaceOrderFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊是由下列的屬性集控制。
+// 變更這些屬性的值即可修改組件的相關
+// 資訊。
+[assembly: AssemblyTitle(KK_StudioSaveWorkspaceOrderFix.KK_StudioSaveWorkspaceOrderFix.PLUGIN_NAME)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(KK_StudioSaveWorkspaceOrderFix.KK_StudioSaveWorkspaceOrderFix.PLUGIN_NAME)]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 將 ComVisible 設為 false 可對 COM 元件隱藏
+// 組件中的類型。若必須從 COM 存取此組件中的類型，
+// 的類型，請在該類型上將 ComVisible 屬性設定為 true。
+[assembly: ComVisible(false)]
+
+// 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
+[assembly: Guid("4f3e6d68-d3b2-4a0f-817a-272eaafb91aa")]
+
+// 組件的版本資訊由下列四個值所組成: 
+//
+//      主要版本
+//      次要版本
+//      組建編號
+//      修訂編號
+//
+// 您可以指定所有的值，也可以使用 '*' 將組建和修訂編號
+// 設為預設，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KK_StudioSaveWorkspaceOrderFix.KK_StudioSaveWorkspaceOrderFix.PLUGIN_VERSION)]
+[assembly: AssemblyInformationalVersion(KK_StudioSaveWorkspaceOrderFix.KK_StudioSaveWorkspaceOrderFix.PLUGIN_RELEASE_VERSION)]
