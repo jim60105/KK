c15b5efe172a5d967bb555e25707a5b0fdfa4ff3
陳鈞 <jim60105@gmail.com>
2020-06-22 02:20:00 +0800

    v20.06.22.0 KK_SSCOG Code重建和整理

--- KK_StudioSimpleColorOnGirls/KK_StudioSimpleColorOnGirls.cs
+++ KK_StudioSimpleColorOnGirls/KK_StudioSimpleColorOnGirls.cs
@@ -36,8 +36,8 @@ namespace KK_StudioSimpleColorOnGirls {
     public class KK_StudioSimpleColorOnGirls : BaseUnityPlugin {
         internal const string PLUGIN_NAME = "Studio Simple Color On Girls";
         internal const string GUID = "com.jim60105.kk.studiosimplecolorongirls";
-        internal const string PLUGIN_VERSION = "20.06.21.1";
-        internal const string PLUGIN_RELEASE_VERSION = "1.0.7.2";
+        internal const string PLUGIN_VERSION = "20.06.22.0";
+        internal const string PLUGIN_RELEASE_VERSION = "1.0.7.3";
 
         internal static new ManualLogSource Logger;
         public void Start() {
@@ -46,9 +46,15 @@ namespace KK_StudioSimpleColorOnGirls {
                 Logger.LogError("This Plugin is not working without Darkness.");
                 return;
             }
-            if (!OobaseCheck()) {
-                return;
-            }
+
+            GameObject go = null;
+            try {
+                go = Patches.CreateSilhouetteGameObjects();
+                if (null == go || go.GetAllChildren().Count != 2) {
+                    Logger.LogError("Your chara/oo_base/p_cm_body_00 is damaged or not a supported version.");
+                    return;
+                }
+            } finally { GameObject.Destroy(go); }
 
             Harmony harmonyInstance = HarmonyWrapper.PatchAll(typeof(Patches));
             harmonyInstance.Patch(
@@ -61,12 +67,85 @@ namespace KK_StudioSimpleColorOnGirls {
                 typeof(MPCharCtrl).GetNestedType("OtherInfo", BindingFlags.Public).GetMethod("UpdateInfo", AccessTools.all),
                 postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.UpdateInfoPostfix)));
         }
+    }
+
+    class Patches {
+        //private static readonly ManualLogSource Logger = KK_StudioSimpleColorOnGirls.Logger;
+        //Copy Simple Color Functions to Female
+        private static MPCharCtrl.StateButtonInfo colorBtn;
+        private static OCIChar ociChar;
+
+        public static void UpdateInfoPostfix(MPCharCtrl.OtherInfo __instance, OCIChar _char) {
+            MPCharCtrl.StateToggleInfo tgl = (MPCharCtrl.StateToggleInfo)__instance.GetField("single");
+            tgl.active = true;
+            tgl.toggle.isOn = _char.GetVisibleSimple();
+
+            MPCharCtrl.StateButtonInfo btn = (MPCharCtrl.StateButtonInfo)__instance.GetField("color");
+            btn.active = true;
+            btn.buttons[0].image.color = _char.oiCharInfo.simpleColor;
+            colorBtn = btn;
+
+            __instance.Invoke("SetSimpleColor", new object[] { _char.oiCharInfo.simpleColor });
+            ociChar = _char;
+            //Logger.LogDebug("Chara Status Info Updated");
+        }
+
+        public static void OnValueChangedSimplePostfix(object __instance, bool _value) {
+            if (__instance.GetProperty("isUpdateInfo") is bool b && b) {
+                return;
+            }
+            ociChar.oiCharInfo.visibleSimple = _value;
+            ociChar.charInfo.fileStatus.visibleSimple = _value;
+            //Logger.LogDebug("Set Visible Simple:" + ociChar.oiCharInfo.visibleSimple);
+        }
+
+        public static void OnValueChangeSimpleColorPostfix(Color _color) {
+            //base.ociChar.SetSimpleColor(_color);
+            ociChar.charInfo.ChangeSimpleBodyColor(_color);
+
+            //otherInfo.SetSimpleColor(_color);
+            if (null != colorBtn) {
+                colorBtn.buttons[0].image.color = _color;
+            }
+            //Logger.LogDebug("Set Simple Color:" + ociChar.oiCharInfo.simpleColor.ToString());
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(AddObjectFemale), "Add", new Type[] { typeof(ChaControl), typeof(OICharInfo), typeof(ObjectCtrlInfo), typeof(TreeNodeObject), typeof(bool), typeof(int) })]
+        private static void AddPostFix(ref OCICharFemale __result, OICharInfo _info) {
+            __result.charInfo.fileStatus.visibleSimple = _info.visibleSimple;
+            __result.charInfo.ChangeSimpleBodyColor(_info.simpleColor);
+        }
+
+        //When loading body, also load unity gameobjects of simply body from male asset 
+        [HarmonyPostfix, HarmonyPatch(typeof(ChaReference), "CreateReferenceInfo")]
+        public static void CreateReferenceInfoPostfix(ChaReference __instance, ulong flags, GameObject objRef) {
+            if (flags >= 1UL && flags <= 15UL && (int)(flags - 1UL) == 2) {
+                Dictionary<ChaReference.RefObjKey, GameObject> dictRefObj = __instance.GetField("dictRefObj").ToDictionary<ChaReference.RefObjKey, GameObject>();
+
+                /* cf_o_root
+                 * └ n_silhouetteTop    (SimpleTop)
+                 *   └ n_body_silhouette
+                 *   └ n_tang_silhouette */
+                GameObject SimpleTop = CreateSilhouetteGameObjects();
+                SimpleTop.transform.SetParent(objRef.FindChild("cf_o_root").transform);
+
+                editDict(ChaReference.RefObjKey.S_SimpleTop, SimpleTop);
+                editDict(ChaReference.RefObjKey.S_SimpleBody, SimpleTop.FindChild("n_body_silhouette"));
+                editDict(ChaReference.RefObjKey.S_SimpleTongue, SimpleTop.FindChild("n_tang_silhouette"));
+                void editDict(ChaReference.RefObjKey key, GameObject newGO) {
+                    if (dictRefObj.TryGetValue(key, out GameObject oldGO) && null != oldGO) { GameObject.Destroy(oldGO); }
+                    dictRefObj[key] = newGO;
+                }
+
+                __instance.SetField("dictRefObj", dictRefObj);
+            }
+        }
 
         /// <summary>
-        /// Check whether oo_base/p_cm_body_00 can be used.
+        /// Create Simple gameobjects.
         /// </summary>
-        /// <returns>True if usable</returns>
-        private static bool OobaseCheck() {
+        /// <returns>False if failed</returns>
+        static internal GameObject CreateSilhouetteGameObjects() {
             GameObject simpleBodyGameObject = null;
             try {
                 simpleBodyGameObject = CommonLib.LoadAsset<GameObject>("chara/oo_base.unity3d", "p_cm_body_00", true, Singleton<Manager.Character>.Instance.mainManifestName);
@@ -74,127 +153,8 @@ namespace KK_StudioSimpleColorOnGirls {
                 FindAssist findAssist = new FindAssist();
                 findAssist.Initialize(simpleBodyGameObject.transform);
 
-                if ((new string[] { "n_silhouetteTop", "n_body_silhouette", "n_tang_silhouette" }).Any(str => null == findAssist.GetObjectFromName(str))) {
-                    throw new Exception();
-                }
-            } catch (Exception) {
-                Logger.LogError("Your chara/oo_base/p_cm_body_00 is damaged or not a supported version.");
-                return false;
-            } finally {
-                GameObject.Destroy(simpleBodyGameObject);
-            }
-
-            return true;
-        }
-    }
-
-    class Patches {
-        private static readonly ManualLogSource Logger = KK_StudioSimpleColorOnGirls.Logger;
-        //Copy Simple Color Functions to Female
-        private static MPCharCtrl.StateButtonInfo colorBtn;
-        private static OCIChar ociChar;
-
-        public static void UpdateInfoPostfix(MPCharCtrl.OtherInfo __instance, OCIChar _char) {
-            FieldInfo[] fieldInfo = __instance.GetType().GetFields();
-            foreach (FieldInfo fi in fieldInfo) {
-                //Logger.LogDebug("Name: " + fi.Name);
-                //Logger.LogDebug("FieldType: " + fi.FieldType);
-                try {
-                    if (fi.Name == "single") {
-                        MPCharCtrl.StateToggleInfo o = (MPCharCtrl.StateToggleInfo)fi.GetValue(__instance);
-                        o.active = true;
-                        o.toggle.isOn = _char.GetVisibleSimple();
-                    } else if (fi.Name == "color") {
-                        MPCharCtrl.StateButtonInfo o2 = (MPCharCtrl.StateButtonInfo)fi.GetValue(__instance);
-                        o2.active = true;
-                        o2.buttons[0].image.color = _char.oiCharInfo.simpleColor;
-                        colorBtn = o2;
-                    }
-                } catch (Exception e) {
-                    Logger.LogError("Exception: " + e);
-                    Logger.LogError("Exception: " + e.Message);
-                }
-            }
-            MethodInfo methodInfo = __instance.GetType().GetMethod("SetSimpleColor");
-            methodInfo.Invoke(__instance, new object[] { _char.oiCharInfo.simpleColor });
-            ociChar = _char;
-            Logger.LogDebug("Chara Status Info Updated");
-        }
-
-        public static void OnValueChangedSimplePostfix(object __instance, bool _value) {
-            if ((bool)__instance.GetProperty("isUpdateInfo")) {
-                return;
-            }
-            ociChar.oiCharInfo.visibleSimple = _value;
-            ociChar.charInfo.fileStatus.visibleSimple = _value;
-            Logger.LogDebug("Set Visible Simple:" + ociChar.oiCharInfo.visibleSimple);
-        }
-
-        public static void OnValueChangeSimpleColorPostfix(MPCharCtrl __instance, Color _color) {
-            //base.ociChar.SetSimpleColor(_color);
-            ociChar.charInfo.ChangeSimpleBodyColor(_color);
-            //ChangeSimpleBodyColorPrefix(_color);  //Debug logging
-            Logger.LogDebug("Set Simple Color:" + ociChar.oiCharInfo.simpleColor.ToString());
-
-            //otherInfo.SetSimpleColor(_color);
-            if (null != colorBtn) {
-                colorBtn.buttons[0].image.color = _color;
-            }
-        }
-
-        //When loading body, also load unity gameobjects of simply body from male asset 
-        [HarmonyPostfix, HarmonyPatch(typeof(ChaReference), "CreateReferenceInfo")]
-        public static void CreateReferenceInfoPostfix(ChaReference __instance, ulong flags, GameObject objRef) {
-            if (flags >= 1UL && flags <= 15UL && (int)(flags - 1UL) == 2) {
-                GameObject simpleBodyGameObject = CommonLib.LoadAsset<GameObject>("chara/oo_base.unity3d", "p_cm_body_00", true, Singleton<Manager.Character>.Instance.mainManifestName);
-                FindAssist findAssist = new FindAssist();
-                findAssist.Initialize(simpleBodyGameObject.transform);
-                Dictionary<ChaReference.RefObjKey, GameObject> dicRefObj = __instance.GetField("dictRefObj").ToDictionary<ChaReference.RefObjKey, GameObject>();
-
-                /* cf_o_root
-                 * └ n_silhouetteTop
-                 *   └ n_body_silhouette
-                 *   └ n_tang_silhouette
-                 */
-                GameObject SimpleTop = findAssist.GetObjectFromName("n_silhouetteTop");
-                GameObject SimpleBody = findAssist.GetObjectFromName("n_body_silhouette");
-                GameObject SimpleTang = findAssist.GetObjectFromName("n_tang_silhouette");
-
-                doMain(dicRefObj, ChaReference.RefObjKey.S_SimpleTop, SimpleTop, objRef.FindChild("cf_o_root"));
-                doMain(dicRefObj, ChaReference.RefObjKey.S_SimpleBody, SimpleBody, SimpleTop);
-                doMain(dicRefObj, ChaReference.RefObjKey.S_SimpleTongue, SimpleTang, SimpleTop);
-                __instance.SetField("dictRefObj", dicRefObj);
-
-                GameObject.Destroy(simpleBodyGameObject);
-            }
-            return;
-
-            void doMain(Dictionary<ChaReference.RefObjKey, GameObject> dic, ChaReference.RefObjKey key, GameObject newGameObject, GameObject newParent) {
-                if (null == newGameObject || null == newParent) return;
-                if (dic.TryGetValue(key, out GameObject go) && null != go) { GameObject.Destroy(go); }
-                newGameObject.transform.SetParent(newParent.transform);
-                dic[key] = newGameObject;
-            }
-        }
-
-        //Update simple color for female's add function
-        private static ChaFileStatus tempStatus = null;
-        [HarmonyPostfix, HarmonyPatch(typeof(AddObjectAssist), "UpdateState")]
-        private static void UpdateStatePostfix(ChaFileStatus _status) {
-            tempStatus = _status;
-        }
-
-        [HarmonyPostfix, HarmonyPatch(typeof(AddObjectFemale), "Add", new Type[] { typeof(ChaControl), typeof(OICharInfo), typeof(ObjectCtrlInfo), typeof(TreeNodeObject), typeof(bool), typeof(int) })]
-        private static void AddPostFix(ref OCICharFemale __result, OICharInfo _info) {
-            __result.charInfo.fileStatus.visibleSimple = _info.visibleSimple;
-            __result.charInfo.ChangeSimpleBodyColor(_info.simpleColor);
-            if (null != tempStatus) {
-                AddObjectAssist.UpdateState(__result, tempStatus);
-                //Logger.LogDebug("Simple Color Patch Finish");
-            } else {
-                Logger.LogError("Update State FAILD: Can't get charFileStatus!");
-            }
-            return;
+                return findAssist.GetObjectFromName("n_silhouetteTop");
+            } finally { GameObject.Destroy(simpleBodyGameObject); }
         }
     }
 }
