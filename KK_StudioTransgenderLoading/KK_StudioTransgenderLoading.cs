d720d942143b66605b7356ecb49afa01e872da5a
陳鈞 <jim60105@gmail.com>
2020-07-27 18:44:06 +0800

    v20.07.27.0 KK_StudioAllGirlsPlugin -> KK_StudioTransgenderLoading SAG淘汰，新版為單純的跨性別替換功能 STL和SAG衝突，請移除SAG

--- KK_StudioTransgenderLoading/KK_StudioTransgenderLoading.cs
+++ KK_StudioTransgenderLoading/KK_StudioTransgenderLoading.cs
@@ -18,47 +18,34 @@ MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 */
 
 using BepInEx;
-using BepInEx.Configuration;
-using BepInEx.Harmony;
-using BepInEx.Logging;
 using Extension;
 using HarmonyLib;
-using MessagePack;
 using Studio;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using UniRx;
 using UnityEngine.UI;
 
-namespace KK_StudioAllGirlsPlugin {
+namespace KK_StudioTransgenderLoading {
     [BepInPlugin(GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     [BepInProcess("CharaStudio")]
-    public class KK_StudioAllGirlsPlugin : BaseUnityPlugin {
-        internal const string PLUGIN_NAME = "Studio All Girls Plugin";
-        internal const string GUID = "com.jim60105.kk.studioallgirlsplugin";
-        internal const string PLUGIN_VERSION = "19.11.02.0";
-		internal const string PLUGIN_RELEASE_VERSION = "1.3.2";
+    [BepInIncompatibility("com.jim60105.kk.studioallgirlsplugin")]
+    public class KK_StudioTransgenderLoading : BaseUnityPlugin {
+        internal const string PLUGIN_NAME = "Studio Transgender Loading";
+        internal const string GUID = "com.jim60105.kk.studiotransgenderloading";
+        internal const string PLUGIN_VERSION = "20.07.27.0";
+        internal const string PLUGIN_RELEASE_VERSION = "0.0.0";
 
-        public static ConfigEntry<bool> Enable { get; private set; }
-
-        internal static new ManualLogSource Logger;
-        public void Awake() {
-            Logger = base.Logger;
-            Enable = Config.AddSetting("Config", "Enable", true, "Load all boys as girls.\n(Restart the game to make this effect)");
-            if (Enable.Value) {
-                HarmonyWrapper.PatchAll(typeof(Patches));
-            }
-        }
-
+        public void Awake() => Harmony.CreateAndPatchAll(typeof(Patches));
     }
 
     class Patches {
+        #region Pose
         //PauseCtrl(PoseCtrl), 取消姿勢讀取的性別檢核
         [HarmonyTranspiler, HarmonyPatch(typeof(PauseCtrl), "CheckIdentifyingCode")]
         public static IEnumerable<CodeInstruction> CheckIdentifyingCodeTranspiler(IEnumerable<CodeInstruction> instructions) {
-            var codes = new List<CodeInstruction>(instructions);
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++) {
                 if (codes[i].opcode == OpCodes.Beq) //Line 23 
                 {
@@ -89,7 +76,7 @@ namespace KK_StudioAllGirlsPlugin {
             }
             _char.ActiveKinematicMode(OICharInfo.KinematicMode.FK, __instance.enableFK, true);
             foreach (KeyValuePair<int, ChangeAmount> keyValuePair2 in __instance.dicFK) {
-                _char.oiCharInfo.bones.TryGetValue(keyValuePair2.Key, out var value);
+                _char.oiCharInfo.bones.TryGetValue(keyValuePair2.Key, out OIBoneInfo value);
                 value?.changeAmount.Copy(keyValuePair2.Value, true, true, true);
             }
             for (int k = 0; k < __instance.expression.Length; k++) {
@@ -97,35 +84,28 @@ namespace KK_StudioAllGirlsPlugin {
             }
             return false;
         }
+        #endregion
 
+        #region CharaList
         //將男女的變更角色讀取都導向這裡，取消讀取的性別檢核
         [HarmonyPrefix, HarmonyPatch(typeof(CharaList), "ChangeCharaFemale")]
-        public static bool ChangeCharaFemale(object __instance) {
-            return ChangeCharaPrefix(__instance);
-        }
+        public static bool ChangeCharaFemale(CharaList __instance) => ChangeChara(__instance);
         [HarmonyPrefix, HarmonyPatch(typeof(CharaList), "ChangeCharaMale")]
-        public static bool ChangeCharaMale(object __instance) {
-            return ChangeCharaPrefix(__instance);
-        }
-        public static bool ChangeCharaPrefix(object __instance) {
-            OCIChar[] array = (from v in Singleton<GuideObjectManager>.Instance.selectObjectKey
-                               select Studio.Studio.GetCtrlInfo(v) as OCIChar into v
-                               where v != null
-                               select v).ToArray();
-            int num = array.Length;
-            for (int i = 0; i < num; i++) {
-                array[i].ChangeChara((__instance.GetField("charaFileSort") as CharaFileSort).selectPath);
-                if ((int)((CharaList)__instance).GetField("sex") == 0) {
-                    KK_StudioAllGirlsPlugin.Logger.LogInfo($"{array[i].charInfo.name} is a girl now!");
-                }
-            }
+        public static bool ChangeCharaMale(CharaList __instance) => ChangeChara(__instance);
+        public static bool ChangeChara(CharaList __instance) {
+            (from v in Singleton<GuideObjectManager>.Instance.selectObjectKey
+             select Studio.Studio.GetCtrlInfo(v) as OCIChar into v
+             where v != null
+             select v).ToList().ForEach((OCIChar ocichar) => {
+                 ocichar.charInfo.fileParam.sex = (byte)(int)__instance.GetField("sex");
+                 ocichar.ChangeChara((__instance.GetField("charaFileSort") as CharaFileSort).selectPath);
+             });
             return false;
         }
 
-        //左側角色清單的OnSelect
         [HarmonyTranspiler, HarmonyPatch(typeof(CharaList), "OnSelectChara")]
         public static IEnumerable<CodeInstruction> OnSelectCharaTranspiler(IEnumerable<CodeInstruction> instructions) {
-            var codes = new List<CodeInstruction>(instructions);
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++) {
                 if (codes[i].opcode == OpCodes.Brfalse) //Line 25
                 {
@@ -144,11 +124,13 @@ namespace KK_StudioAllGirlsPlugin {
             }
             return codes.AsEnumerable();
         }
+        #endregion
 
         //右側物品清單的OnSelect
+        #region Workspace
         [HarmonyTranspiler, HarmonyPatch(typeof(CharaList), "OnSelect")]
         public static IEnumerable<CodeInstruction> OnSelectTranspiler(IEnumerable<CodeInstruction> instructions) {
-            var codes = new List<CodeInstruction>(instructions);
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++) {
                 if (codes[i].opcode == OpCodes.Stloc_1 && codes[i - 1].opcode == OpCodes.Isinst) //Line 30
                 {
@@ -167,17 +149,16 @@ namespace KK_StudioAllGirlsPlugin {
             if (!Singleton<Studio.Studio>.IsInstance()) {
                 return false;
             }
-            OCIChar[] self = (from v in Singleton<GuideObjectManager>.Instance.selectObjectKey
-                              select Studio.Studio.GetCtrlInfo(v) as OCIChar into v
-                              where v != null
-                              select v).ToArray<OCIChar>();
-            ((Button)__instance.GetField("buttonChange")).interactable = !self.IsNullOrEmpty<OCIChar>();
+            ((Button)__instance.GetField("buttonChange")).interactable = !(from v in Singleton<GuideObjectManager>.Instance.selectObjectKey
+                                                                           select (Studio.Studio.GetCtrlInfo(v) as OCIChar) into v
+                                                                           where v != null
+                                                                           select v).ToArray().IsNullOrEmpty();
             return false;
         }
 
         [HarmonyTranspiler, HarmonyPatch(typeof(CharaList), "OnDelete")]
         public static IEnumerable<CodeInstruction> OnDeleteTranspiler(IEnumerable<CodeInstruction> instructions) {
-            var codes = new List<CodeInstruction>(instructions);
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
             for (int i = 0; i < codes.Count; i++) {
                 if (codes[i].opcode == OpCodes.Stloc_0 && codes[i - 1].opcode == OpCodes.Isinst) //Line 30
                 {
@@ -187,44 +168,6 @@ namespace KK_StudioAllGirlsPlugin {
             }
             return codes.AsEnumerable();
         }
-
-        //將所有男性讀取為女性
-        //將AddMale重定向至AddFemale
-        [HarmonyPrefix, HarmonyPatch(typeof(Studio.Studio), "AddMale")]
-        public static bool AddMalePrefix(string _path) {
-            Singleton<Studio.Studio>.Instance.AddFemale(_path);
-            KK_StudioAllGirlsPlugin.Logger.LogInfo($"{_path} is a girl now!");
-            return false;
-        }
-
-        //Studio.AddObjectAssist處的角色讀取，將性別檢核拿掉，只以女性讀取
-        [HarmonyPrefix, HarmonyPatch(typeof(AddObjectAssist), "LoadChild", new Type[] { typeof(ObjectInfo), typeof(ObjectCtrlInfo), typeof(TreeNodeObject) })]
-        public static bool LoadChildPrefix(ObjectInfo _child, ObjectCtrlInfo _parent = null, TreeNodeObject _parentNode = null) {
-            if (_child.kind == 0) {
-                OICharInfo oicharInfo = _child as OICharInfo;
-                var female = AddObjectFemale.Load(oicharInfo, _parent, _parentNode);
-
-                if (oicharInfo.sex == 0) {
-                    KK_StudioAllGirlsPlugin.Logger.LogInfo($"{female.charInfo.name} is a girl now!");
-                    oicharInfo.SetProperty("sex", 1);
-                }
-                return false;
-            }
-            return true;
-        }
-
-        [HarmonyPostfix, HarmonyPatch(typeof(ChaFile), "SetParameterBytes")]
-        public static void SetParameterBytesPostfix(ChaFile __instance, byte[] data) {
-            ChaFileParameter chaFileParameter = MessagePackSerializer.Deserialize<ChaFileParameter>(data);
-
-            //There's no exType before EC_Yoyaku
-            if (null != typeof(ChaFileParameter).GetProperty("exType")) {
-                chaFileParameter.SetProperty("exType", 0);
-            }
-            chaFileParameter.sex = 1;
-
-            __instance.parameter.Copy(chaFileParameter);
-            return;
-        }
+        #endregion
     }
 }
