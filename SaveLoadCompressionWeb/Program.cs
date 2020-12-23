52a33e5c23c9c245afecd1168f3b9deae1dbfd28
陳鈞 <jim60105@gmail.com>
2020-12-24 05:54:07 +0800

    增加下載按鈕

--- SaveLoadCompressionWeb/Program.cs
+++ SaveLoadCompressionWeb/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using BlazorDownloadFile;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -16,6 +17,7 @@ namespace SaveLoadCompressionWeb {
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped(typeof(KK_SaveLoadCompression.SaveLoadCompression));
             builder.Services.AddSingleton(sp => (IJSInProcessRuntime)sp.GetRequiredService<IJSRuntime>());
+            builder.Services.AddBlazorDownloadFile();
 
             await builder.Build().RunAsync();
         }
