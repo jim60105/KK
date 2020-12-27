85dfbbaf17ff60c7e8a4d676e5fd4591c58167eb
陳鈞 <jim60105@gmail.com>
2020-12-27 13:41:30 +0800

    以appsettings.json來管理Log Level

--- SaveLoadCompressionWeb/Program.cs
+++ SaveLoadCompressionWeb/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using BlazorDownloadFile;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
@@ -12,7 +13,7 @@ namespace SaveLoadCompressionWeb {
         public static async Task Main(string[] args) {
             WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            builder.Logging.SetMinimumLevel(LogLevel.Debug);
+            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped(typeof(KK_SaveLoadCompression.SaveLoadCompression));
