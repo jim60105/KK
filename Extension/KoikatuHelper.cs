577bf5c38b03920fa16cabe2eb207210f1dfd1ea
陳鈞 <jim60105@gmail.com>
2021-08-29 04:05:38 +0800

    Extension: Fix TryGetPluginInstance() NullRefException

--- Extension/KoikatuHelper.cs
+++ Extension/KoikatuHelper.cs
@@ -4,13 +4,14 @@ using BepInEx;
 namespace Extension {
     class KoikatuHelper {
         public static BaseUnityPlugin TryGetPluginInstance(string pluginName, Version minimumVersion = null) {
-            BepInEx.Bootstrap.Chainloader.PluginInfos.TryGetValue(pluginName, out PluginInfo target);
-            if (null != target) {
+            minimumVersion = minimumVersion ?? new Version(0, 0, 0);
+            if(BepInEx.Bootstrap.Chainloader.PluginInfos.TryGetValue(pluginName, out PluginInfo target) 
+               && null != target) {
                 if (target.Metadata.Version >= minimumVersion) {
                     return target.Instance;
                 }
-                Logger.LogWarning($"{pluginName} v{target.Metadata.Version.ToString()} is detacted OUTDATED.");
-                Logger.LogWarning($"Please update {pluginName} to at least v{minimumVersion.ToString()} to enable related feature.");
+                Logger.LogWarning($"{pluginName} v{target.Metadata.Version} is detacted OUTDATED.");
+                Logger.LogWarning($"Please update {pluginName} to at least v{minimumVersion} to enable related feature.");
             }
             return null;
         }
