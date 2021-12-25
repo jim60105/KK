447d8cd54ac8125943555e2d881de5784170c5fa
陳鈞 <jim60105@gmail.com>
2021-12-26 02:12:56 +0800

    Add batch decompress tool
    我不擔保它不會損壞存檔
    我不公開release此工具，但將publish加入git
    想使用的人自行取用、Use at your own risk.

---
+++ SaveLoadCompression.DecompressTool/Program.cs
@@ -0,0 +1,40 @@
+﻿Extension.Logger.logger = null;
+PngCompression.PngCompression pngCompression = new();
+List<Task> tasks = new();
+DirectoryInfo decompressDirectory = Directory.CreateDirectory("decompress_" + DateTime.Now.Ticks);
+
+Console.WriteLine($"Start decompression in parallel.");
+Console.WriteLine($"All png files will be output to {decompressDirectory.FullName}");
+
+var files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.png");
+foreach (string file in files)
+{
+    tasks.Add(Task.Run(() =>
+    {
+        string target = Path.Combine(decompressDirectory.FullName, Path.GetFileName(file));
+        try
+        {
+            Console.WriteLine($"Start decompression: {Path.GetFileName(file)}");
+            if (0 != pngCompression.Load(file, target))
+            {
+                Console.WriteLine($"Finish decompression: {Path.GetFileName(file)}");
+            }
+            else
+            {
+                Console.WriteLine($"Copy not compressed file: {Path.GetFileName(file)}");
+                File.Copy(file, target, true);
+            }
+        }
+        catch (FileLoadException)
+        {
+            Console.WriteLine($"Skip not support file: {Path.GetFileName(file)}");
+            File.Copy(file, target, true);
+        };
+    }));
+}
+
+Task.WaitAll(tasks.ToArray());
+
+Console.WriteLine($"Processed {files.Length} files.");
+Console.ReadKey();
+
